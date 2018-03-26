48399e463418349ea7d3f3abb7366ea5c9e88857
AVM <miranska@yahoo.com>
2018-03-26 18:55:24 -0400

    Factor out address to interface

--- angular-play/src/app/components/user/user.component.ts
+++ angular-play/src/app/components/user/user.component.ts
@@ -9,12 +9,7 @@ export class UserComponent implements OnInit {
 
   name : string;
   title : string;
-  address : {
-    street : string,
-    city : string,
-    province : string
-  }
-
+  address : Address;
   constructor() { 
     console.log("In constructor");
   }
@@ -30,4 +25,10 @@ export class UserComponent implements OnInit {
     }
   }
 
+}
+
+interface Address {
+  street : string,
+  city : string,
+  province : string
 }
