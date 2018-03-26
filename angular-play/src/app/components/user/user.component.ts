f88a0e3471a50c76756005b6f3e6fff4e07c37e1
AVM <miranska@yahoo.com>
2018-03-26 18:35:42 -0400

    Adding attributes to class User

--- angular-play/src/app/components/user/user.component.ts
+++ angular-play/src/app/components/user/user.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
+  name = 'Jane';
+  title = 'Developer';
+  address = {
+    street : 'Church St.',
+    city : 'Toronto',
+    province: 'ON'
+  }
+
   constructor() { }
 
   ngOnInit() {
