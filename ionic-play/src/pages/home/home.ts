2eea5bfa775e509060e426ac61f7283e36b3774a
AVM <miranska@yahoo.com>
2018-04-02 20:18:24 -0400

    Add Observable link to Firebase and add CRUD operations as per https://github.com/angular/angularfire2/blob/master/docs/rtdb/lists.md

--- ionic-play/src/pages/home/home.ts
+++ ionic-play/src/pages/home/home.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+
 import { DetailPage } from '../detail/detail';
 
 @Component({
@@ -11,7 +16,12 @@ export class HomePage {
  
   items: any[];
 
-  constructor(public navCtrl: NavController) {
+  tagsRef: AngularFireList<any>;
+  tags: Observable<any[]>;
+
+
+  constructor(public navCtrl: NavController, 
+    db: AngularFireDatabase) {
     this.items = [];
     //populate a list of items with dummy records
     for(let i = 0; i < 10; i++){
@@ -21,6 +31,18 @@ export class HomePage {
       })
     }
 
+    // let's get some attributes fro persistent storage
+    this.tagsRef = db.list('messages');
+    // Use snapshotChanges().map() to store the key
+    this.tags = this.tagsRef.snapshotChanges().map(changes => {
+      // the '...' operator will "extract"/spread the attributes from c.payload.val(),
+      // making it easier to access these attributes
+      // See https://reactjs.org/docs/jsx-in-depth.html#spread-attributes
+      // for details
+      return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    });
+
+
   }
 
   itemSelected(item){
@@ -29,6 +51,19 @@ export class HomePage {
     this.navCtrl.push(DetailPage,
       { item: item }
     );
+  }
+
+  addTag(newName: string) {
+    this.tagsRef.push({ text: newName });
+  }
+  updateTag(key: string, newText: string) {
+    this.tagsRef.update(key, { text: newText });
+  }
+  deleteTag(key: string) {    
+    this.tagsRef.remove(key); 
+  }
+  deleteAllTags() {
+    this.tagsRef.remove();
   }
 
 }
